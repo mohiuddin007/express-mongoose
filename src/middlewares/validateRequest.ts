33df0a9f25d26b7166264d467d9ad97fdb31d559
mohiuddin007 <mohiuddinmazumder94@gmail.com>
2023-12-02 18:58:45 +0600

    academic faculty crud add

--- src/middlewares/validateRequest.ts
+++ src/middlewares/validateRequest.ts
@@ -4,10 +4,10 @@ import { AnyZodObject } from "zod";
 const validateRequest = (schema: AnyZodObject) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
+      console.log(req.body)
       await schema.parseAsync({
         body: req.body,
       });
-      // console.log(req.body, "end");
       next();
     } catch (err) {
       next(err);
---
+++ src/modules/academicFaculty/academicFaculty.route.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import validateRequest from "../../middlewares/validateRequest";
+import { AcademicFacultyValidation } from "./academicFaculty.validation";
+import { AcademicFacultyControllers } from "./academicFaculty.controller";
+
+const router = Router();
+
+router.get("/", AcademicFacultyControllers.getAllAcademicFaculty);
+router.post(
+  "/create-academic-faculty",
+  validateRequest(
+    AcademicFacultyValidation.createAcademicFacultyValidationSchema
+  ),
+  AcademicFacultyControllers.createAcademicFaculty
+);
+router.get("/:facultyId", AcademicFacultyControllers.getSingleAcademicFaculty);
+router.patch(
+  "/:facultyId",
+  validateRequest(
+    AcademicFacultyValidation.updateAcademicFacultyValidationSchema
+  ),
+  AcademicFacultyControllers.updateAcademicFaculty
+);
+
+export const AcademicFacultyRouters = router;
