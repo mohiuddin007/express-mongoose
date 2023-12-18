9beb1e3fca183de1045786dcc7ff9acc33916937
mohiuddin007 <mohiuddinmazumder94@gmail.com>
2023-12-18 17:39:41 +0600

    access token verify

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -5,34 +5,24 @@ import AppError from "../errors/AppError";
 import httpStatus from "http-status";
 import config from "../config";
 
-
 const auth = () => {
-  return catchAsync(
-    async (req: Request, res: Response, next: NextFunction) => {
-      const token = req.headers.authorization;
-      //is the token is sent form the client
-      if (!token) {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization;
+    //is the token is sent form the client
+    if (!token) {
+      throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized!");
+    }
+
+    //check if the token is valid
+    jwt.verify(token, config.jwt_secret_key as string, function (err, decoded) {
+      if (err) {
         throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized!");
       }
 
-      //check if the token is valid
-      jwt.verify(
-        token,
-        config.jwt_secret_key as string,
-        function (err, decoded) {
-          if (err) {
-            throw new AppError(
-              httpStatus.UNAUTHORIZED,
-              "You are not authorized!"
-            );
-          }
-
-          req.user = decoded as JwtPayload;
-        }
-      );
+      req.user = decoded as JwtPayload;
       next();
-    }
-  );
+    });
+  });
 };
 
 export default auth;
