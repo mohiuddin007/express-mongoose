576afd59d9b053d0f094c6d6d63f1adbbe3ec8b5
mohiuddin007 <mohiuddinmazumder94@gmail.com>
2024-01-21 00:54:35 +0600

    cors issue fix

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import cookieParser from "cookie-parser";
 const app: Application = express();
 
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:5173"] }));
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 app.use(cookieParser());
 
 //application routes
