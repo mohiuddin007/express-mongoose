8675c5f4add2baefb081b11197d051f868f61807
mohiuddin007 <mohiuddinmazumder94@gmail.com>
2024-01-21 16:31:38 +0600

    auth guard

--- src/modules/academicSemester/academicSemester.route.ts
+++ src/modules/academicSemester/academicSemester.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AcademicSemesterControllers } from "./academicSemester.controller";
 import validateRequest from "../../middlewares/validateRequest";
 import { AcademicSemesterValidation } from "./academicSemester.validation";
+import auth from "../../middlewares/auth";
 
 const router = Router();
 
@@ -13,7 +14,7 @@ router.post(
   AcademicSemesterControllers.createAcademicSemester
 );
 
-router.get("/", AcademicSemesterControllers.getAllAcademicSemesters);
+router.get("/", auth("admin"), AcademicSemesterControllers.getAllAcademicSemesters);
 
 router.get(
   "/:semesterId",
